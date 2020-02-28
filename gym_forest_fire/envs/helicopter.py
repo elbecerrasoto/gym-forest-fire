dd6bbd438e705c59a6cb8cb8f06e9f0c94f4e8cf
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2020-02-28 16:23:58 -0600

    env.reset() Fix

--- gym_forest_fire/envs/helicopter.py
+++ gym_forest_fire/envs/helicopter.py
@@ -17,47 +17,52 @@ import seaborn as sns
 # Dependencies
 import forest_fire
 
-# Magic command to output the window at Ipython interactive terminal
-# %matplotlib qt
-# tree = '|', empty = '.', fire = '*'
-# tree = 3, empty = 2, fire = 1
-#ord('|')
-#ord('.')
-#ord('*')
+# Features to add
+
+# Action info attribute, DONE
+# Rocks, dead cells
+# Rock boundary
+# Custom starting positions
+# Water feature
 
 class Helicopter(forest_fire.ForestFire):
     """ Helicopter class """
     def __init__(self, pos_row = None, pos_col = None, freeze = None, water = 100,
                  n_row = 16, n_col = 16,
                  p_tree=0.100, p_fire=0.001, p_init_tree=0.75,
-                 boundary='reflective', tree = '|', empty = '.', fire = '*'):
+                 boundary='reflective', tree = 3, empty = 1, fire = 7):
         super().__init__(n_row,n_col,
              p_tree,p_fire,p_init_tree,
              boundary,tree,empty,fire)
         # Helicopter attributes
+        self.actions_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+        self.actions_cardinality = len(self.actions_set)
         if pos_row is None:
+            # Start aprox in the middle
             self.pos_row = math.ceil(self.n_row/2) - 1
         else:
             self.pos_row = pos_row
         if pos_col is None:
+            # Start aprox in the middle
             self.pos_col = math.ceil(self.n_col/2) - 1
         else:
             self.pos_col = pos_col      
         if freeze is None:
+            # Move aprox a 1/4 of the grid
             self.freeze = math.ceil((self.n_row + self.n_col) / 4)
         else:
             self.freeze = freeze
         self.defrost = self.freeze
+        # Water not yet implemented
         self.water = water
+        # Number of Times where a fire was extinguished
         self.hits = 0
-        self.current_reward = 0
         # Render Info
+        self.current_reward = 0
         self.color_tree = np.array([15, 198, 43, int(1.0*255)]) # Green RGBA
         self.color_empty = np.array([255, 245, 166, int(1.0*255)]) # Beige RGBA
         self.color_fire = np.array([255, 106, 58, int(1.0*255)]) # Red RGBA
-        self.color_helicopter = np.array([64, 18, 1, int(1.0*255)]) # Brown RGBA
         self.grid_to_rgba()
-        sns.set_style('whitegrid')
     def step(self, action):
         """Must return tuple with
         numpy array, int reward, bool termination, dict info
@@ -71,6 +76,7 @@ class Helicopter(forest_fire.ForestFire):
             # Don't delay the reward
             # Reward it often
             reward = self.calculate_reward()
+            self.current_reward = reward
             return (obs, reward, termination, {'hits': self.hits})
         if self.defrost == 0:
             # Run fire simulation
@@ -136,7 +142,7 @@ class Helicopter(forest_fire.ForestFire):
         return hit
     def reset(self):
         # Another random grid
-        self.__init__(self.pos_row,self.pos_col,
+        self.__init__(None,None,
                       self.freeze,self.water,
                       self.n_row,self.n_col,
                       self.p_tree,self.p_fire,self.p_init_tree,
@@ -159,6 +165,8 @@ class Helicopter(forest_fire.ForestFire):
         self.rgba_mat = rgba_mat
         return rgba_mat
     def render(self):
+        # Plot style
+        sns.set_style('whitegrid')
         # Main Plot
         plt.imshow(self.grid_to_rgba(), aspect='equal')
         # Title showing Reward
@@ -187,3 +195,5 @@ class Helicopter(forest_fire.ForestFire):
         fig = plt.gcf()
         plt.show()
         return fig
+    def close():
+        print('Gracefully Exiting, come back soon')
